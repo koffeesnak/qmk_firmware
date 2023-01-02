5fe9e7c75047f9fe58eda0e0e0756e1305141a61
coffeesnake <philip.rud@gmail.com>
2023-01-02 14:32:12 -0800

    Fix idobao id75

--- keyboards/idobao/id75/keymaps/koffeesnak/keymap.c
+++ keyboards/idobao/id75/keymaps/koffeesnak/keymap.c
@@ -23,27 +23,27 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_5x15(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX, XXXXXXX,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,
-   LT(2,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,
-   LT(1,KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, XXXXXXX,  KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,
-        KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    XXXXXXX, XXXXXXX,  KC_B,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,      KC_SLSH,
-        KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX,KC_BSPC, XXXXXXX, KC_DEL,  XXXXXXX, KC_LEFT, KC_DOWN,    KC_RGHT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,
+   LT(2,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,
+   LT(1,KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,  KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,
+        KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    _______, _______,  KC_B,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,      KC_SLSH,
+        KC_LCTL, _______, KC_LALT, KC_LGUI, _______, KC_SPC,  _______, _______,KC_BSPC, _______, KC_DEL,  _______, KC_LEFT, KC_DOWN,    KC_RGHT
     ),
 
     [1] = LAYOUT_ortho_5x15(
-        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_RBRC, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, KC_GRV,  KC_PENT,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RBRC, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,  XXXXXXX, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_END
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        _______, _______, _______, _______, _______, KC_GRV,  _______, _______, _______, _______, KC_UP,   _______, _______, KC_RBRC, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GRV,  KC_PENT,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RBRC, _______, _______, KC_PGUP, _______,
+        _______, _______, _______, _______, _______, KC_ENT,  _______, _______, KC_DEL,  _______, _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
 
     [2] = LAYOUT_ortho_5x15(
-        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAD, RGB_VAI,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAD, RGB_VAI,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______, _______, _______, _______
     )
 };
 
